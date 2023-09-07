1ef46ea4c55093d971010debf432462c0848b08f
Casper Kaun <casper.g.kaun@gmail.com>
2023-09-08 01:39:25 +0800

    - Updated test_crypto_string variable

--- sample_test/sample_variables.py
+++ sample_test/sample_variables.py
@@ -1,3 +1,3 @@
 something = 'Fish and Chips'
 GOOGLE_URL = 'https://www.google.com/'
-TEST_CRYPTO_STRING = 'crypt:7zDpBKsS86P10ezvssC251NhP3ENIKdKHwi2cMF8YVxjnST6+awRCjQBriZebHrnAlMgYA=='
+TEST_CRYPTO_STRING = 'crypt:vcxQ98XhXoUI41AKPXK6IwtljqpcgRyWe62Gd8wuJlcxhqZOrrW3mFhUbR9Fqu+GaWocWA=='
